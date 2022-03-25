1a52c80f016b526023d503462d286a1d0e69ab64
HermonYurandaCruz <hermondacruz73@gmail.com>
2022-03-25 23:31:59 +0200

    v1

---
+++ app/src/main/java/com/codeline/logoinicio/TelaCadastro.java
@@ -0,0 +1,25 @@
+package com.codeline.logoinicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class TelaCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+        getSupportActionBar().hide();
+    }
+
+    /// abrir tela login
+    public void AbrirTelaLogin(View view){
+        Intent intent=new Intent(TelaCadastro.this,TelaLogin.class);
+        startActivity(intent);
+
+
+    }
+}
--- app/src/main/java/com/codeline/logoinicio/TelaInicio.java
+++ app/src/main/java/com/codeline/logoinicio/TelaInicio.java
@@ -2,15 +2,40 @@ package com.codeline.logoinicio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TelaInicio extends AppCompatActivity {
-
+private TextView tv_telaInicio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicio);
         getSupportActionBar().hide();
 
+
+        //abrir tela cadastro
+        IniciarCadasgtro();
+        tv_telaInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(TelaInicio.this,TelaCadastro.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    private void IniciarCadasgtro() {tv_telaInicio=findViewById(R.id.Tv_novaConta);}
+
+    ///abrir tela login
+    public void AbrirTelaLogin(View view){
+        Intent intent=new Intent(TelaInicio.this,TelaLogin.class);
+        startActivity(intent);
+
+
+    }
+
 }
